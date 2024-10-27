cc8dc810894da274574faa87c3948438453486e2
nadeem <nadeemasgar18@gmail.com>
2024-10-28 00:37:10 +0530

    working with event

--- 05. basics-03-events-starting-code/app.js
+++ 05. basics-03-events-starting-code/app.js
@@ -1,9 +1,28 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      name: "",
+      confirmedName: "",
+      counter: 10,
     };
+  },
+  methods: {
+    add() {
+      this.counter++;
+    },
+    reduce() {
+      this.counter--;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    submitForm() {
+      alert("Submitted");
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
